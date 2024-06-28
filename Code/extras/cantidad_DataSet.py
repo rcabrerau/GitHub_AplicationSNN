2e24d58f957a1f603b6a54652baaca99d588a2a2
Robinson Cabrera <rcabreraur@uoc.edu>
2024-06-28 00:13:54 -0500

    final

--- Code/extras/cantidad_DataSet.py
+++ Code/extras/cantidad_DataSet.py
@@ -1,7 +1,8 @@
 import h5py
 
 # Abre el archivo HDF5 en modo lectura
-with h5py.File("E:\MASTER UOC\AULAS_4TO_SEMESTRE\TFM\AplicationSNN\datasetConvertion_CNN_to_SNN\SPIKING_labels\spiking_data_labels_16062024_0029.h5", "r") as f:                          
+#with h5py.File("E:/MASTER UOC/AULAS_4TO_SEMESTRE/TFM/AplicationSNN/datasetConvertion_CNN_to_SNN/SPIKING_labels/spiking_data_labels_16062024_0029.h5", "r") as f:                          
+with h5py.File("E:/MASTER UOC/AULAS_4TO_SEMESTRE/TFM/AplicationSNN/datasetConvertion_CNN_to_SNN/SPIKING_labels/spiking_data_labels_22062024_1529_test.h5", "r") as f:                           
     # Itera sobre los grupos del archivo
     total_datos = 0
     for group_name in f:
